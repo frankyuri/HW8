bea3dd69ba0a15c58fee0126315dca8b3cc766f2
frankyuri <61720277+frankyuri@users.noreply.github.com>
2020-12-26 14:50:00 +0800

    Add files via upload

--- HW8.cpp
+++ HW8.cpp
@@ -1,5 +1,4 @@
-// HW8.cpp : ©w¸q¥D±±¥xÀ³¥Îµ{¦¡ªº¶i¤JÂI¡C
-//
+ï»¿//#include "stdafx.h"
 #include <iostream>
 #include <string>
 #include <stack>
@@ -8,73 +7,93 @@
 
 using namespace std;
 
-void input(string sMessage, string &sInputString)
+void input(string sMessage, string& sInputString)
 {
 	do {
 		cout << sMessage << endl;
 		cin >> sInputString;
-	} while ( cin.fail() );
+	} while (cin.fail());
 
 	return;
 }
 
 void pushAndCorrectDeque(string& aString, deque<char>& aDeque)
 {
-	for ( int i = 0; i < aString.size(); i++) {
-		// ¥[¤Jµ{¦¡½X§PÂ_ aString[ i ] ¬O§_¬° '<'
-		// ­Y¬O«h±Ë±ó¡A¨Ã±N³Ì«á¤@­Ó©ñ¤J aDeque ªº¦r¥À®ø°£
-		if ( aString[ i ] == '<' ) {
-			
 
-		// add your code here
-
+	for (int i = 0; i < aString.size(); i++) {
+	
 		
+		if (aString[i] == '<') {
+			if (!aDeque.empty()) aDeque.pop_back();
 		}
-		// ­Y¤£¬O«h±N aString[ i ] ©ñ¤J aDeque ªº§ÀºÝ
 		else {
-
-
-			// add your code here
-
-
+			aDeque.push_back(aString[i]);
 		}
 	}
-		// ª`·N¡G¤£¥i¥H¦A«Å§i©w¸q¥ô¦óÅÜ¼Æ©Îª«¥ó
+	
 }
 
 void pushAndCorrectStackQueue(string& aString, stack<char>& aStack, queue<char>& aQueue)
 {
-	for ( int i = 0; i < aString.size(); i++) {
+	for (int i = 0; i < aString.size(); i++) {
 
-
-	// add your code here
+		if (aString[i] == '<') {
+			if (!aQueue.empty()) {
+				for (int q = 1; q <aQueue.size(); q++) {
+					aQueue.push(aQueue.front());
+					aQueue.pop();
+				}
+				aQueue.pop();
+				aStack.pop();
+			}
+		}
+		else
+		{
+			aQueue.push(aString[i]);
+			aStack.push(aString[i]);
+		}
+		// add your code here
 
 
 	}
+
+
 }
 
 bool isPalindromeDeque(deque<char> aDeque)
 {
-	// µM«á§Q¥Î aDeque ªº operations:
-	// aDeque.size(), aDeque.empty(),
-	// aDeque.back(), aDeque.front(),
-	// aDeque.pop_back(), aDeque.pop_front(),
-	// ¨Ó§PÂ_ palindrome¡A¨Ã±N aDeque ²MªÅ
-
-	// ª`·N¡G¤£¥i¥H¦A«Å§i©w¸q¥ô¦óÅÜ¼Æ©Îª«¥ó
-
-
-	// add your code here
-
+	
+	while (1) {
+		if (aDeque.size() == 0 || aDeque.size() == 1) {
+			return true;
+		}
+		if (aDeque.front() == aDeque.back()) {
+			aDeque.pop_front();
+			aDeque.pop_back();
+		}
+		else {
+			break;
+		}
+	}
 
 	return false;
 }
 
 bool isPalindromeStackQueue(stack<char> aStack, queue<char> aQueue)
 {
+	while (1) {
 
-
-	// add your code here
+		if ( aQueue.size() == 0) {
+			return true;
+		}
+		else if (aStack.top() == aQueue.front()) {
+			aStack.pop();
+			aQueue.pop();
+		}
+		else {
+			break;
+		}
+	}
 
 
 	return false;
@@ -82,123 +101,125 @@ bool isPalindromeStackQueue(stack<char> aStack, queue<char> aQueue)
 
 int main()
 {
- 	string sInput;
+	string sInput;
 
 	// Queue STL
-   queue<char> myQueue;
+	queue<char> myQueue;
 
-   for ( char j = 0; j < 5; j++ )
-	   myQueue.push( j + 'A' );
+	for (char j = 0; j < 5; j++)
+		myQueue.push(j + 'A');
 
-   cout << "myQueue is a true queue:  ";
-   
-   while ( !myQueue.empty() )  {
-	   cout << myQueue.front() << " ";
-	   myQueue.pop();
-   }
+	cout << "myQueue is a true queue:  ";
 
-   cout << endl;
+	while (!myQueue.empty()) {
+		cout << myQueue.front() << " ";
+		myQueue.pop();
+	}
+
+	cout << endl;
 
 	// Use Deque STL as Queue
-   deque<char> myDeque;
-   
-   for ( char j = 0; j < 5; j++ )
-	   myDeque.push_back( j + 'A' );
+	deque<char> myDeque;
 
-   cout << "myDeque is also a queue:  ";
-   
-   while ( !myDeque.empty() )  {
-	   cout << myDeque.front() << " ";
-	   myDeque.pop_front();
-   }
+	for (char j = 0; j < 5; j++)
+		myDeque.push_back(j + 'A');
 
-   cout << endl;
+	cout << "myDeque is also a queue:  ";
 
-   for ( char j = 0; j < 5; j++ )
-	   myDeque.push_front( j + 'A'  );
+	while (!myDeque.empty()) {
+		cout << myDeque.front() << " ";
+		myDeque.pop_front();
+	}
 
-   cout << "myDeque is still a queue: ";
+	cout << endl;
 
-   while ( !myDeque.empty() )  {
-	   cout << myDeque.back() << " ";
-	   myDeque.pop_back();
-   }
+	for (char j = 0; j < 5; j++)
+		myDeque.push_front(j + 'A');
 
-   cout << endl;
+	cout << "myDeque is still a queue: ";
+
+	while (!myDeque.empty()) {
+		cout << myDeque.back() << " ";
+		myDeque.pop_back();
+	}
+
+	cout << endl;
 
 
-   // Stack STL
+	// Stack STL
 
-   stack<char> myStack;
+	stack<char> myStack;
 
-   for ( char j = 0; j < 5; j++ )
-		myStack.push(  j + 'A'  );
+	for (char j = 0; j < 5; j++)
+		myStack.push(j + 'A');
 
-   cout << "myStack is a true stack:  ";
+	cout << "myStack is a true stack:  ";
 
-   while ( !myStack.empty() )  {
-	   cout << myStack.top() << " ";
-	   myStack.pop();
-   }
+	while (!myStack.empty()) {
+		cout << myStack.top() << " ";
+		myStack.pop();
+	}
 
-   cout << endl;
+	cout << endl;
 
 	// Use Deque STL as Stack
 
-   for ( char j = 0; j < 5; j++ )
-	   myDeque.push_front(  j + 'A'  );
+	for (char j = 0; j < 5; j++)
+		myDeque.push_front(j + 'A');
 
-   cout << "myDeque is also a stack:  ";
-   
-   while ( !myDeque.empty() )  {
-	   cout << myDeque.front() << " ";
-	   myDeque.pop_front();
-   }  
+	cout << "myDeque is also a stack:  ";
 
-   cout << endl;
+	while (!myDeque.empty()) {
+		cout << myDeque.front() << " ";
+		myDeque.pop_front();
+	}
 
-   for ( char j = 0; j < 5; j++ )
-	   myDeque.push_back(  j + 'A'  );
+	cout << endl;
 
-   cout << "myDeque is still a stack: ";
-   
-   while ( !myDeque.empty() )  {
-	   cout << myDeque.back() << " ";
-	   myDeque.pop_back();
-   }
+	for (char j = 0; j < 5; j++)
+		myDeque.push_back(j + 'A');
 
-   cout << endl;
+	cout << "myDeque is still a stack: ";
 
-   // ª`·N¡G©³¤Uµ{¦¡½X¤£¥i¥H§ó°Ê
+	while (!myDeque.empty()) {
+		cout << myDeque.back() << " ";
+		myDeque.pop_back();
+	}
+
+	cout << endl;
+
+	
 
 	while (1) {
-		input( "\nEnter a string or X to Exit:", sInput );
+		input("\nEnter a string or X to Exit:", sInput);
 
-		if ( sInput.size() == 1 && ( sInput == "X" || sInput == "x" ) )
-			return 0;	
+		if (sInput.size() == 1 && (sInput == "X" || sInput == "x"))
+			return 0;
 
-		pushAndCorrectDeque( sInput, myDeque );
-		bool yes = isPalindromeDeque( myDeque );
+		pushAndCorrectDeque(sInput, myDeque);
+		bool yes = isPalindromeDeque(myDeque);
 
-		for ( int i = myDeque.size(); i > 0; i--) {
+		for (int i = myDeque.size(); i > 0; i--) {
 			cout << myDeque.front();
 			myDeque.pop_front();
 		}
 
-		if ( yes ) 
+		if (yes)
+
 			cout << " is a palindrome" << endl;
 		else
+
 			cout << " is not a palindrome" << endl;
 
-		pushAndCorrectStackQueue( sInput, myStack, myQueue );
-		yes = isPalindromeStackQueue( myStack, myQueue );
+		pushAndCorrectStackQueue(sInput, myStack, myQueue);
+		yes = isPalindromeStackQueue(myStack, myQueue);
 
-		for ( int i = myQueue.size(); i > 0; i--) {
+		for (int i = myQueue.size(); i > 0; i--) {
 			cout << myQueue.front();
 			myQueue.pop();
 		}
 
-		if ( yes ) 
+		if (yes)
 			cout << " is a palindrome" << endl;
 		else
 			cout << " is not a palindrome" << endl;
